130d30677e1865dec5670c6dfdfd9aae195e8ad2
Justin Reyes <justin.regi.reyes@gmail.com>
2015-11-27 18:13:39 -0800

    added validation and erd between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	has_many :items
+	has_many :categories
+
+	belongs_to :item
+
+	validates :email, :username, :password_digest, presence: true
+	validates :email, :username, uniqueness: true
+
+	def self.confirm (params)
+		@user = User.find_by({email: params[:username]})
+		@user.try(:authenticate, params[:password])
+	end
 end
