2c54f4986f14dd2458217e908b08480b14958845
Justin Reyes <justin.regi.reyes@gmail.com>
2015-11-30 16:08:29 -0800

    generated user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/delete'
+
   get '/', to: "welcome#index"
 end
