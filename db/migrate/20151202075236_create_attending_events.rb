e1fe3abf1a304e14a1801a3cce38448b76816c37
Justin Reyes <justin.regi.reyes@gmail.com>
2016-05-09 17:32:25 -0700

    added going, undecided, and declined attrs to attending_events

--- db/migrate/20151202075236_create_attending_events.rb
+++ db/migrate/20151202075236_create_attending_events.rb
@@ -3,6 +3,10 @@ class CreateAttendingEvents < ActiveRecord::Migration
     create_table :attending_events do |t|
       t.integer :user_id
       t.integer :event_id
+      t.boolean :going
+      t.boolean :maybe
+      t.boolean :declined
+      t.boolean :undecided, default: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160504182248) do
   create_table "attending_events", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "going"
+    t.boolean  "maybe"
+    t.boolean  "declined"
+    t.boolean  "undecided",  default: true
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "categories", force: :cascade do |t|
