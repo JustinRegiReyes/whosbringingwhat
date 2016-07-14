3928b54cba55336ec1ae274f1225c906c75037ef
Justin Reyes <justin.regi.reyes@gmail.com>
2016-07-14 01:07:37 -0700

    fixed undefined error if multiple events shared the same day

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -4,6 +4,7 @@ $(document).on('page:load', clndrPage);
 
 function clndrPage() {
     calendar();
+    window.test = [1, 2]
 }
 
 function calendar() {
@@ -14,12 +15,14 @@ function calendar() {
     // Here's some magic to make sure the dates are happening this month.
     var thisMonth    = moment().format('YYYY-MM');
     var nextMonth    = moment().add(1, 'month').format('YYYY-MM');
+    var twoMonth    = moment().add(2, 'month').format('YYYY-MM');
     // Events to load into calendar
     var eventArray = [
         {
             title: 'Camping',
             date_end: thisMonth + '-14',
             date_start: thisMonth + '-10',
+            date: thisMonth + '-10',
             description: 'description', id: 1,
             user: {
                 username: "Roro"
@@ -27,44 +30,110 @@ function calendar() {
         }, {
             date_end: thisMonth + '-23',
             date_start: thisMonth + '-10',
+            date: thisMonth + '-10',
             title: 'Board Game Night', description: 'description', id: 2,
             user: {
                 username: "Roro"
             }
         }, {
             date_start: thisMonth + '-27',
+            date: thisMonth + '-27',
             title: 'Single Day Event', description: 'description', id: 3,
+            user: {
+                username: "Roro"
+            }
+        }, {
+            date_start: nextMonth + '-17',
+            date: nextMonth + '-17',
+            title: 'Single Day Event', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        }, 
+        {
+            date_start: nextMonth + '-17',
+            date: nextMonth + '-17',
+            title: 'Working Event', description: 'description', id: 7,
             user: {
                 username: "Roro"
             }
         },
         {
-            date_start: nextMonth + '+17',
-            title: 'Single Day Event', description: 'description', id: 7,
+            date_start: nextMonth + '-18',
+            date: nextMonth + '-18',
+            title: 'Working Event', description: 'description', id: 7,
             user: {
                 username: "Roro"
             }
         },
-        { date_start: thisMonth + '-' + '10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats', description: 'description', id: 4,
+        {
+            date_start: nextMonth + '-18',
+            date: nextMonth + '-18',
+            title: 'Working Event', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        },
+        {
+            date_start: twoMonth + '-19',
+            date: twoMonth + '-19',
+            title: 'Next Event', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        },
+        {
+            date_start: twoMonth + '-19',
+            date: twoMonth + '-19',
+            title: 'Next Event', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        }, 
+        {
+            date_start: twoMonth + '-20',
+            date: twoMonth + '-20',
+            title: 'Fish Tacos', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        },
+        {
+            date_start: twoMonth + '-20',
+            date: twoMonth + '-20',
+            title: 'Fish Tacos', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        },
+        {
+            date_start: twoMonth + '-21',
+            date: twoMonth + '-21',
+            title: 'Fish Tacos', description: 'description', id: 7,
+            user: {
+                username: "Roro"
+            }
+        }, 
+        { date_start: thisMonth + '-10', date: thisMonth + '-10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats', description: 'description', id: 4,
         user: {
             username: "Roro"
         } },
-        { date_start: thisMonth + '-' + '19', title: 'Cat Frisbee', location: 'Jefferson Park', description: 'description', id: 5,
+        { date_start: thisMonth + '-19', date: thisMonth + '-19', title: 'Cat Frisbee', location: 'Jefferson Park', description: 'description', id: 5,
         user: {
             username: "Roro"
         } },
-        { date_start: thisMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats', description: 'description', id: 6,
+        { date_start: thisMonth + '-23', date: thisMonth + '-19', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats', description: 'description', id: 6,
         user: {
             username: "Roro"
         } },
-        { date_start: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography', description: 'description', id: 7,
+        { date_start: nextMonth + '-07', date: nextMonth + '-07', title: 'Small Cat Photo Session', location: 'Center for Cat Photography', description: 'description', id: 7,
         user: {
             username: "Roro"
         } }
     ];
     // sort the eventArray based on its date_start
     eventArray.sort(function(a, b){
-      if (a.date_start > b.date_start) {
+      if (a.date_start >= b.date_start) {
         return 1;
       }
       if (a.date_start < b.date_start) {
@@ -248,8 +317,6 @@ function eventDaysCompiler(events, dates) {
         var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][monthNum];
         var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][dayNum];
         var eventDate = day + " " + theDate;
-        console.log(dayNum,'dayNum');
-        console.log(theDate);
 
         $('div[class*=' + date + ']').append(eventDaysContainerOpen + eventDate + eventDaysContainerClose);
     });
