cb96363142be90ffed322a007fef64239143520d
Justin Reyes <justin.regi.reyes@gmail.com>
2016-07-16 18:23:08 -0700

    needed the previously thought useless key

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_many :items
 	# renaming has_many :events alias to created_events
 	has_many :created_events, foreign_key: "user_id", class_name: "Event"
+	has_many :attending_events
 
 	# renaming has_many :events throgh: :attending_events alias to going_tos
 	has_many :going_tos, foreign_key: "user_id", through: :attending_events, source: :event
