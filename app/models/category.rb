c1e017ec7e25d36e15be8226fd531301c6c72a2e
Justin Reyes <justin.regi.reyes@gmail.com>
2016-08-15 10:11:17 -0700

    added class method enough_items? to see if theres enough items for the needed attr

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
 	has_many :items
 	belongs_to :event
+
+	def enough_items?
+		if  self.items.length >= self.needed
+			return true
+		else
+			return false
+		end
+	end
 end
