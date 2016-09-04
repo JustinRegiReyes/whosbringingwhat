33dd89d35fb7f8d094358d6bd9c2463d15ff6eed
Justin Reyes <justin.regi.reyes@gmail.com>
2016-09-04 02:02:07 -0700

    made rando items helper for examples for what to biring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   include SessionsHelper
+  include ItemsHelper
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,6 @@
 module ItemsHelper
+	def rando_item
+		items = ["Poker Set", "Mac N Cheese", "Cups", "Drankz", "Beer", "Chips", "Wall Streamers", "Cheesecake", "Charcoal", "Grill"]
+		return items.sample
+	end
 end
