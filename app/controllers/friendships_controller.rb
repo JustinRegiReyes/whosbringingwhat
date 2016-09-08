55486684cb63dc95e93ece88441de2ed429554c2
Justin Reyes <justin.regi.reyes@gmail.com>
2016-09-07 23:28:22 -0700

    added create and destroy methods

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,19 @@
+class FriendshipsController < ApplicationController
+	def create
+		@friendship = Friendship.new({:friend_id: params[:friend_id]})
+		if @friendship.save
+			flash[:notice] = "Added friend."
+			redirect_to root_url
+		else
+			flash[:error] = "Unable to add friend."
+			redirect_to root_url
+		end
+	end
+
+	def destroy
+		@friendship = current_user.friendships.find(params[:id])
+		@friendship.destroy
+		flash[:notice] = "Removed friendship."
+		redirect_to "/home"
+	end
+end
