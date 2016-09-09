48ab79a6352f48f18c92b43e4906595b155e7fda
Justin Reyes <justin.regi.reyes@gmail.com>
2016-09-08 17:24:05 -0700

    added model methods to find records based on various ids

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,37 @@
 class Notification < ActiveRecord::Base
 	belongs_to :user
+
+	has_many :created_events, foreign_key: "user_id", class_name: "Event"
+
+	def guest
+		User.find_by_id(self.guest_id)
+	end
+
+	def friend
+		User.find_by_id(self.friendship_id)
+	end
+
+	def comment
+		Comment.find_by_id(self.comment_id)
+	end
+
+	def event
+		Event.find_by_id(self.event_id)
+	end
+
+	def invited_by
+		User.find_by_id(self.invited_by_id)
+	end
+
+	# def reference
+	# 	if self.type == "guest"
+	# 		self.guest
+	# 	elsif self.type == "friend"
+	# 		self.friend
+	# 	elsif self.type == "comment"
+	# 		self.comment
+	# 	elsif self.type == "event"
+	# 		self.event
+	# 	end
+	# end
 end
