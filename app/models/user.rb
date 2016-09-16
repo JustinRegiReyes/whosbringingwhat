744d5b56e90b8334c2b3f1204f3c2385a1fa0df3
Justin Reyes <justin.regi.reyes@gmail.com>
2016-09-15 19:01:30 -0700

    fixed is_friend method. And inverse_friends to be only friendships that are accepted

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 	has_many :friendships
 	has_many :friends, -> {where(friendships: {accepted: true})}, :through => :friendships
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+	has_many :inverse_friends, -> {where(friendships: {accepted: true})}, :through => :inverse_friendships, :source => :user
 
 	has_secure_password
 	validates :username, :password_digest, presence: true
