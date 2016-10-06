06f68996443fdfb27656c8b6f346f80bfe5d7ed3
Justin Reyes <justin.regi.reyes@gmail.com>
2016-10-05 23:17:51 -0700

    Added length validations to username, from, and bio

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :username, :password_digest, :email, presence: true
 	validates_uniqueness_of :email, :case_sensitive => false
+	validates :username, length: { in: 1..15 }
+	validates :from, length: { in: 1..20 }
+	validates :bio, length: { in: 1..100 }
 
 	# paperclip
 
