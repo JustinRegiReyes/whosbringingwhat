ea3ce17b193bb3453fff531b5e8d9a8ea8b6052d
Justin Reyes <justin.regi.reyes@gmail.com>
2016-11-09 21:05:53 -0800

    Notifications of comment are destroyed if comment is deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
 
 	after_create :notify_event_goers
 
+	before_destroy :destroy_notification
+
 	def notify_event_goers
 		eventId = self.event_id
 		attendingUsers = User.joins(:attending_events).where({attending_events: {event_id: eventId, going: true}}).where.not({users: {id: self.user_id}})
@@ -19,5 +21,12 @@ class Comment < ActiveRecord::Base
 
 	def owner?(user)
 		self.user_id == user.id
+	end
+
+	def destroy_notification
+		notification = Notification.find_by_comment_id(self.id)
+		if notification
+			notification.destroy
+		end
 	end
 end
